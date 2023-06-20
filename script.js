23067641bc6226d6fdce257b408d9980d8f5fbf3
ryotaro <ryotaro@tripla.jp>
2023-06-21 03:08:15 +0900

    feat:improve algorithm

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-document.getElementById('calculate-btn').addEventListener('click', function() {
-    var propertyIncome = document.getElementById('property-income').value;
-    var salaryIncome = document.getElementById('salary-income').value;
-    var otherIncome = document.getElementById('other-income').value;
-    var propertyExpenses = document.getElementById('property-expenses').value;
-    var insuranceDeduction = document.getElementById('insurance-deduction').value;
-    var lifeInsuranceDeduction = document.getElementById('life-insurance-deduction').value;
-    var earthquakeInsuranceDeduction = document.getElementById('earthquake-insurance-deduction').value;
+function calculateTaxableIncome() {
+    var salary = document.getElementById('salary').value;
+    var propertyIncome = document.getElementById('propertyIncome').value;
+    var propertyExpenses = document.getElementById('propertyExpenses').value;
+    var otherIncome = document.getElementById('otherIncome').value;
+    var socialInsurance = document.getElementById('socialInsurance').value;
+    var lifeInsurance = document.getElementById('lifeInsurance').value;
+    var earthquakeInsurance = document.getElementById('earthquakeInsurance').value;
 
-    var totalIncome = Number(propertyIncome) + Number(salaryIncome) + Number(otherIncome) - Number(propertyExpenses) - Number(insuranceDeduction) - Number(lifeInsuranceDeduction) - Number(earthquakeInsuranceDeduction);
+    var taxableIncome = (Number(salary) + Number(propertyIncome) + Number(otherIncome)) - 
+                        (Number(propertyExpenses) + Number(socialInsurance) + Number(lifeInsurance) + Number(earthquakeInsurance));
+    
     var tax;
-
-    if(totalIncome <= 1950000) {
-        tax = totalIncome * 0.05;
-    } else if (totalIncome <= 3300000) {
-        tax = totalIncome * 0.1 - 97500;
-    } else if (totalIncome <= 6950000) {
-        tax = totalIncome * 0.2 - 427500;
-    } else if (totalIncome <= 9000000) {
-        tax = totalIncome * 0.23 - 636000;
-    } else if (totalIncome <= 18000000) {
-        tax = totalIncome * 0.33 - 1536000;
-    } else if (totalIncome <= 40000000) {
-        tax = totalIncome * 0.40 - 2796000;
+    if (taxableIncome <= 1950000) {
+        tax = taxableIncome * 0.05;
+    } else if (taxableIncome <= 3300000) {
+        tax = taxableIncome * 0.10 - 97500;
+    } else if (taxableIncome <= 6950000) {
+        tax = taxableIncome * 0.20 - 427500;
+    } else if (taxableIncome <= 9000000) {
+        tax = taxableIncome * 0.23 - 636000;
+    } else if (taxableIncome <= 18000000) {
+        tax = taxableIncome * 0.33 - 1536000;
+    } else if (taxableIncome <= 40000000) {
+        tax = taxableIncome * 0.40 - 2796000;
     } else {
-        tax = totalIncome * 0.45 - 4796000;
+        tax = taxableIncome * 0.45 - 4796000;
     }
 
-    document.getElementById('result').innerHTML = "課税所得は " + Math.round(tax) + " 円です";
-});
+    document.getElementById('result').innerHTML = '課税所得は ' + taxableIncome + ' 円で、税金は ' + tax + ' 円です。';
+}
